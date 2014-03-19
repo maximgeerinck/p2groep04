60c50dc3c07e46b9a765e70021c92e39505d73e1
Roy Hollanders <roy_9852@hotmail.com>
2014-03-19 14:34:30 +0100

    Finished implementation of sendMail().

--- src/entity/User.java
+++ src/entity/User.java
@@ -219,7 +219,7 @@ public class User
             transport.close();
             
         } catch (Exception exception) {
-
+            exception.printStackTrace();
         }
         
         
